b94964b02a8ef88f3e26450569f55a69eebb6de9
Namnika <janbandhunamnika@gmail.com>
2022-09-01 11:54:16 +0530

    Update files

---
+++ netlify/functions/routes/todolists.js
@@ -0,0 +1,30 @@
+const router = require("express");
+const body-parser = require("body-parser");
+let Todolist = require("../models/todolist.model");
+
+router.use(bodyParser.urlencoded({extended: true}));
+
+router.route("/").get((req, res) => {
+  Todolist.find()
+  .then(todos => res.json(todos))
+  .catch(err => {res.status(400).json("Error:" + err)});
+});
+
+router.route("/").post((req, res) => {
+  const newTodo = new Todolist({
+    text: req.body.text
+  });
+
+  newTodo.save()
+  .then(() => res.send("Todolist Item added!!"));
+  .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id")
+  .delete((req, res) => {
+    Todolist.findByIdAndDelete(req.params.id)
+    .then(() => {"Todolist Item deleted!!"});
+    .catch(err => {res.status(400).json("Error: " + err)});
+  });
+
+module.exports = router;
